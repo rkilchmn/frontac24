69e1d3823fbb06b8eb1ba3372f4e7947f43a5145
rkilchmn <roger@kilchenmann.net>
2023-09-12 00:54:36 +0000

    language update

---
+++ core/lang/en_US/LC_MESSAGES/en_US-2.4.1-3.php
@@ -0,0 +1,21 @@
+<?php
+return array(
+    '' => 'Project-Id-Version: FrontAccounting 2.4
+Report-Msgid-Bugs-To: 
+POT-Creation-Date: 2017-11-09 22:44+0100
+PO-Revision-Date: 2018-02-18 00:18-0600
+Last-Translator: Translation Team <info@frontaccounting.com>
+Language-Team: Translators <info@frontaccounting.com>
+Language: en_US
+MIME-Version: 1.0
+Content-Type: text/plain; charset=iso-8859-1
+Content-Transfer-Encoding: 8bit
+',
+    'Chequing Account' => 'Checking Account',
+    'Cheque' => 'Check',
+    'TOTAL ORDER VAT INCL.' => 'TOTAL ORDER INCL. TAX',
+    'TOTAL ORDER EX VAT' => 'TOTAL ORDER EX TAX',
+    'TOTAL DELIVERY INCL. VAT' => 'TOTAL DELIVERY INCL. TAX',
+    'Your VAT no.' => 'Your Tax Id.',
+    'Our VAT No.' => 'Our Tax Id.',
+);
